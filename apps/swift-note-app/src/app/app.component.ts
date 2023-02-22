721601e9054abf97bc0b51b13109f0082727bdcf
siddartha1189 <siddharthui22@gmail.com>
2023-02-22 06:42:57 -0800

    Initial commit

---
+++ apps/swift-note-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'swift-note-app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'swift-note-app';
+}
